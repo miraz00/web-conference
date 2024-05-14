418c6993dd9a0c1998bb699a7aa57dca7260b1ef
Miraz J Naina <42742560+miraz00@users.noreply.github.com>
2024-05-14 12:25:43 +0530

    Chat

--- main.js
+++ main.js
@@ -399,6 +399,7 @@ function manageConnection(pc) {
     manageRemoteId(pc)
     manageRemoteName(pc)
     endConnection(pc)
+    manageChat(pc)
 
     pc.onconnectionstatechange = ((event)=>{
         switch(pc.connectionState) {
@@ -539,6 +540,47 @@ function handleClosed(pc) {
     }
 }
 
+function manageChat(pc) {
+    const chatChannel = pc.createDataChannel("chat", {
+        negotiated: true,
+        id: 4
+    });
+
+    chatChannel.onopen = (()=>{
+        pc.chatChannel = chatChannel
+        document.getElementById('sendMessage').addEventListener('click', (event) => {
+            const textBox = document.querySelector('.text-box');
+            const message = textBox.innerText;
+
+            if (message) {
+                // Create a new <li> for self-sent message
+                const selfMessageLi = document.createElement('li');
+                selfMessageLi.classList.add('self');
+                selfMessageLi.textContent = message;
+                document.getElementById('messages').appendChild(selfMessageLi);
+
+                // Send the message to all connected peers
+                for (const key in remoteConnectedPeers) {
+                    // Ensure that the chatChannel exists before sending
+                    if (remoteConnectedPeers[key].chatChannel) {
+                        remoteConnectedPeers[key].chatChannel.send(message);
+                    }
+                }
+
+                // Clear the text box's innerText
+                textBox.innerText = '';
+            }
+        })
+    })
+
+    chatChannel.onmessage = ((event) => {
+        const receivedMessage = event.data;
+        const otherMessageLi = document.createElement('li');
+        otherMessageLi.classList.add('other');
+        otherMessageLi.textContent = `${pc.remoteName}: ${receivedMessage}`
+        document.getElementById('messages').appendChild(otherMessageLi);
+    })
+}
 // -------------------------     utilities      -----------------------------------
 
 function addVideo(stream, user = false, screen = false, pc) {
@@ -685,3 +727,90 @@ nameButton.onclick = ((event) => {
 document.getElementById("hangupButton").onclick = ((event)=>{
     window.location.reload()
 })
+
+// ---------------------- chat ------------------------------------
+
+var element = $('.floating-chat');
+var myStorage = localStorage;
+
+if (!myStorage.getItem('chatID')) {
+    myStorage.setItem('chatID', createUUID());
+}
+
+setTimeout(function() {
+    element.addClass('enter');
+}, 1000);
+
+element.click(openElement);
+
+function openElement() {
+    var messages = element.find('.messages');
+    var textInput = element.find('.text-box');
+    element.find('>i').hide();
+    element.addClass('expand');
+    element.find('.chat').addClass('enter');
+    var strLength = textInput.val().length * 2;
+    textInput.keydown(onMetaAndEnter).prop("disabled", false).focus();
+    element.off('click', openElement);
+    element.find('.header button').click(closeElement);
+    element.find('#sendMessage').click(sendNewMessage);
+    messages.scrollTop(messages.prop("scrollHeight"));
+}
+
+function closeElement() {
+    element.find('.chat').removeClass('enter').hide();
+    element.find('>i').show();
+    element.removeClass('expand');
+    element.find('.header button').off('click', closeElement);
+    element.find('#sendMessage').off('click', sendNewMessage);
+    element.find('.text-box').off('keydown', onMetaAndEnter).prop("disabled", true).blur();
+    setTimeout(function() {
+        element.find('.chat').removeClass('enter').show()
+        element.click(openElement);
+    }, 500);
+}
+
+function createUUID() {
+    // http://www.ietf.org/rfc/rfc4122.txt
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+
+    var uuid = s.join("");
+    return uuid;
+}
+
+function sendNewMessage() {
+    var userInput = $('.text-box');
+    var newMessage = userInput.html().replace(/\<div\>|\<br.*?\>/ig, '\n').replace(/\<\/div\>/g, '').trim().replace(/\n/g, '<br>');
+
+    if (!newMessage) return;
+
+    var messagesContainer = $('.messages');
+
+    messagesContainer.append([
+        '<li class="self">',
+        newMessage,
+        '</li>'
+    ].join(''));
+
+    // clean out old message
+    userInput.html('');
+    // focus on input
+    userInput.focus();
+
+    messagesContainer.finish().animate({
+        scrollTop: messagesContainer.prop("scrollHeight")
+    }, 250);
+}
+
+function onMetaAndEnter(event) {
+    if ((event.metaKey || event.ctrlKey) && event.keyCode == 13) {
+        sendNewMessage();
+    }
+}
